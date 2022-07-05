02ac236a5378267a8f80aa1dafbb867ed11de993
Mickael <noxbike@gmail.com>
2022-07-05 21:52:56 +0400

    navigation add + info by time

--- src/app/component/navigation/navigation.component.ts
+++ src/app/component/navigation/navigation.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import navigation from './navigation.json';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
-
+  navigations = navigation.navigation;
   constructor() { }
 
   ngOnInit(): void {
